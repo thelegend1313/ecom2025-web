56dbc5650a797f59260c5182a2b4422eaad51fdd
thelegend1313 <interiorlegend1313@gmail.com>
2025-07-15 11:11:46 +0700

    t2

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,7 +1,7 @@
 // rafce
 import React from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-//import Home from '../pages/home'
+import Home from '../pages/home'
 import Shop from '../pages/Shop'
 import Cart from '../pages/Cart'
 import History from '../pages/History'
